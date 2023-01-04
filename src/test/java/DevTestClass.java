1ed76bbe98bf7eff94df5b15e6e4fbc9d58461bc
Ibrahim <ibrahim141446@gmail.com>
2023-01-04 15:27:50 +0530

    commit 2

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -1,8 +1,8 @@
 public class DevTestClass {
-
     public static void main(String[] args){
         System.out.println("Hello Man! How are you?");
         //King of Coding Languages
         //creating a pull request change here
+        System.out.println("How was your day?");
     }
 }
